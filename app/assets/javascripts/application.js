e1ef4c6e394ad9123a6c17d15c88fd14385585d4
Klever-Sobrinho <kleverfortaleza@gmail.com>
2019-01-21 09:37:20 -0300

    Add bower, jquery-masked-input and locale pt-BR

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery-maskedinput/src/jquery.maskedinput.js
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+// require turbolinks
 //= require_tree .
